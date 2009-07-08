1d7120984bfb255e2e000bf552e2e05bbd102a35
Theo Cushion <theo@jivatechnology.com>
2009-07-08 16:23:15 +0100

    Feature: Added definition for adding ssh keys

---
+++ ssh/metadata.rb
@@ -0,0 +1,5 @@
+maintainer        "Jiva Technology Limited"
+maintainer_email  "theo@jivatechnology.com"
+license           "Apache 2.0"
+description       "Provides a define to set up ssh authorized keys"
+version           "0.1"
