42143f13a61eaff9d1eab8f7e07e42705ad26d66
Theo Cushion <theo@jivatechnology.com>
2009-07-09 12:20:08 +0100

    Feature: Added git 1.6 support to ubuntu 8.04

--- git/recipes/default.rb
+++ git/recipes/default.rb
@@ -16,6 +16,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+if node[:platform] == "ubuntu" && node[:platform_version] == "8.04"
+  
+  repos = ["deb http://ppa.launchpad.net/smartlounge/ppa/ubuntu hardy main",
+           "deb-src http://ppa.launchpad.net/smartlounge/ppa/ubuntu hardy main"]
+  
+  repos.each do |r|
+    bash "Adding repo #{r}" do
+      code "echo '#{r}' >> /etc/apt/sources.list"
+      not_if "grep '#{r}' /etc/apt/sources.list"
+    end
+  end
+
+  key = "504EBE1344441FF0E7CC4CA30E76E800643C34E6"
+  bash "Add key" do
+    code ["gpg --no-default-keyring --keyring /tmp/sml.keyring --keyserver keyserver.ubuntu.com --recv #{key}",
+          "gpg --no-default-keyring --keyring /tmp/sml.keyring --export --armor #{key} | sudo apt-key add - ",
+          "rm /tmp/sml.keyring",
+          "apt-get update"].join("&&")
+    not_if "apt-key list | grep 'Launchpad PPA for Smartlounge'"
+  end
+  
+end
+
 case node[:platform]
 when "debian", "ubuntu"
   package "git-core"
