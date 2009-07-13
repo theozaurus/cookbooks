c2b03493dd9883418f070796c88fee31a009c0c3
Theo Cushion <theo@jivatechnology.com>
2009-07-13 14:12:24 +0100

    Feature: new_database definition defaults to utf8 now

--- mysql/definitions/mysql_new_database.rb
+++ mysql/definitions/mysql_new_database.rb
@@ -1,7 +1,7 @@
 define :mysql_new_database do
 
   bash "creating database #{params[:name]}" do
-    code   "mysql --user=root --password=#{@node[:mysql][:server_root_password]} -e \"CREATE DATABASE #{params[:name]}\""
+    code   "mysql --user=root --password=#{@node[:mysql][:server_root_password]} -e \"CREATE DATABASE #{params[:name]} CHARACTER SET utf8 COLLATE utf8_general_ci;\""
     not_if "mysql --user=root --password=#{@node[:mysql][:server_root_password]} #{params[:name]} -e exit;"
   end
   
