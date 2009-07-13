8459c7cdc74bdc49cffe0ef8b1e4197697348f59
Theo Cushion <theo@jivatechnology.com>
2009-07-13 16:23:42 +0100

    Feature: Added checksum to rabbitmq download to speed up operation

--- rabbitmq/recipes/default.rb
+++ rabbitmq/recipes/default.rb
@@ -3,6 +3,7 @@ include_recipe "build-essential"
 
 remote_file "/tmp/rabbitmq-server_1.4.0-1_all.deb" do
   source "http://www.rabbitmq.com/releases/rabbitmq-server/v1.4.0/rabbitmq-server_1.4.0-1_all.deb"
+  checksum "a3020818e43f09978e641d62a6c0ba87ffca00888258d71267a9b0c52af9a5ee"
   mode 0644
 end
 
