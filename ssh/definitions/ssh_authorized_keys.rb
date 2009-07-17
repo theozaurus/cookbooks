751786426f2036305744f86d13f4c6cab998bce2
Theo Cushion <theo@jivatechnology.com>
2009-07-17 14:30:12 +0100

    Fix: For setting up ssh key tasks when a user doesn't exist

--- ssh/definitions/ssh_authorized_keys.rb
+++ ssh/definitions/ssh_authorized_keys.rb
@@ -1,9 +1,10 @@
 define :ssh_authorized_keys, :user => nil, :keys => [] do
 
-  home = `cd ~#{params[:user]} && pwd`.strip
-  authorized_keys = "#{home}/.ssh/authorized_keys"
-
-  directory "#{home}/.ssh" do
+  directory "#{params[:user]}/.ssh" do
+    home = `cd ~#{params[:user]} && pwd`.strip
+    authorized_keys = "#{home}/.ssh/authorized_keys"
+    
+    path "#{home}/.ssh"
     owner params[:user]
     group params[:user]
     mode 0700
@@ -11,12 +12,18 @@ define :ssh_authorized_keys, :user => nil, :keys => [] do
   
   params[:keys].each do |key|
     bash "adding key '#{key.split(" ").last}'" do
+      home = `cd ~#{params[:user]} && pwd`.strip
+      authorized_keys = "#{home}/.ssh/authorized_keys"
+      
       code "echo '#{key}' >> #{authorized_keys}"
       not_if "grep '#{key}' #{authorized_keys}"
     end
   end
   
   bash "adjust permissions of authorized_keys file for #{params[:user]}" do
+    home = `cd ~#{params[:user]} && pwd`.strip
+    authorized_keys = "#{home}/.ssh/authorized_keys"
+    
     code "chmod 600 #{authorized_keys} && chown #{params[:user]}:#{params[:user]} #{authorized_keys}"
     not_if "test `find #{home}/.ssh -maxdepth 1 -user #{params[:user]} -group #{params[:user]} -name authorized_keys -perm 600 | wc -l` = 1"
   end
