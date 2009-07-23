306fbf0cf632f5f389cd48a47fe673614513ca8e
Theo Cushion <theo@jivatechnology.com>
2009-07-23 16:21:11 +0100

    Fix: Added bash madness to fix the ssh helper definitions

--- ssh/definitions/ssh_authorized_keys.rb
+++ ssh/definitions/ssh_authorized_keys.rb
@@ -1,24 +1,25 @@
 define :ssh_authorized_keys, :user => nil, :keys => [] do
 
-  home = `cd ~#{params[:user]} && pwd`.strip
-  authorized_keys = "#{home}/.ssh/authorized_keys"
-
-  directory "#{home}/.ssh" do
-    owner params[:user]
-    group params[:user]
-    mode 0700
+  bash "create #{params[:user]}/.ssh" do
+    code "mkdir ~#{params[:user]}/.ssh"
+    not_if "test -d ~#{params[:user]}/.ssh"
   end
   
+  bash "setup permissions on #{params[:user]}/.ssh" do
+    code "chmod 0700 ~#{params[:user]}/.ssh && chown #{params[:user]}:#{params[:user]} ~#{params[:user]}/.ssh"
+    not_if "test `find ~template -maxdepth 1 -type d -perm 0700 -name .ssh -user template -group template | wc -l` = 1"
+  end
+
   params[:keys].each do |key|
     bash "adding key '#{key.split(" ").last}'" do
-      code "echo '#{key}' >> #{authorized_keys}"
-      not_if "grep '#{key}' #{authorized_keys}"
+      code "echo '#{key}' >> ~#{params[:user]}/.ssh/authorized_keys"
+      not_if "grep '#{key}' ~#{params[:user]}/.ssh/authorized_keys"
     end
   end
   
   bash "adjust permissions of authorized_keys file for #{params[:user]}" do
-    code "chmod 600 #{authorized_keys} && chown #{params[:user]}:#{params[:user]} #{authorized_keys}"
-    not_if "test `find #{home}/.ssh -maxdepth 1 -user #{params[:user]} -group #{params[:user]} -name authorized_keys -perm 600 | wc -l` = 1"
+    code "chmod 600 ~#{params[:user]}/.ssh/authorized_keys && chown #{params[:user]}:#{params[:user]} ~#{params[:user]}/.ssh/authorized_keys"
+    not_if "test `find ~#{params[:user]}/.ssh -maxdepth 1 -user #{params[:user]} -group #{params[:user]} -name authorized_keys -perm 600 | wc -l` = 1"
   end
     
 end
--- ssh/definitions/ssh_known_hosts.rb
+++ ssh/definitions/ssh_known_hosts.rb
@@ -1,13 +1,10 @@
 define :ssh_known_hosts, :user => nil, :hosts => [] do
-  
-  home = `cd ~#{params[:user]} && pwd`.strip
-  known_hosts = "#{home}/.ssh/known_hosts"
-  
+    
   bash "create known_hosts" do
     user params[:user]
     group params[:user]
-    code "touch #{known_hosts}"
-    not_if "sudo -u #{params[:user]} file #{known_hosts}"
+    code "touch ~#{params[:user]}/.ssh/known_hosts"
+    not_if "sudo -u #{params[:user]} file ~#{params[:user]}/.ssh/known_hosts"
   end
   
   params[:hosts].each do |host|
