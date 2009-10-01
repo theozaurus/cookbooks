253aa9e1863ab7b689defee5fc9bf3e71a4947bc
Theo Cushion <theo@jivatechnology.com>
2009-10-01 13:52:30 +0100

    Fix: Adding GEM_HOME environment variable to gem.sh, useful for sparse environments

--- rack_app/files/default/gem.sh
+++ rack_app/files/default/gem.sh
@@ -1,7 +1,9 @@
 if [ -d ~/.gem/ruby/1.8/bin ]; then
   export PATH=~/.gem/ruby/1.8/bin:$PATH
+  export GEM_HOME=~/.gem/ruby/1.8
 fi
 
 if [ -d ~/.gem/ruby/1.9/bin ]; then
   export PATH=~/.gem/ruby/1.9/bin:$PATH
+  export GEM_HOME=~/.gem/ruby/1.9
 fi
