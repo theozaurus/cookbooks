21f69a81034d1376b44db75f536eb8ad2503a673
Theo Cushion <theo@jivatechnology.com>
2009-10-21 23:49:03 +0100

    Feature: Added basic thinkingsphinx installer

---
+++ sphinx/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Jiva Technology Limited"
+maintainer_email "theo@jivatechnology.com"
+license          "Apache 2.0"
+description      "Installs Sphin search engine"
+version          "0.1"
+
+
