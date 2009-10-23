41eecd49ecb0f22aa1dba7522bd323f70e608801
Theo Cushion <theo@jivatechnology.com>
2009-10-23 12:17:49 +0100

    Fix: Bundler now no longer required rdoc or ri

--- bundler/recipes/default.rb
+++ bundler/recipes/default.rb
@@ -1,6 +1,7 @@
 include_recipe "git"
 
 bash "Download and install bundler" do
-  code "git clone git://github.com/wycats/bundler.git && cd bundler && rake install && cd .. && rm -rf bundler"
+  cwd "/tmp"
+  code "git clone git://github.com/wycats/bundler.git && cd bundler && rake package && gem install pkg/bundler-0.7.0.pre.gem --no-rdoc --no-ri && cd .. && rm -rf bundler"
   not_if "gem specification bundler"
 end
