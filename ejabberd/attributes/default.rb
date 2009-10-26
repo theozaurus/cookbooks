c4c101d41c65f65fb81ff02d909f2ad0533edcca
Theo Cushion <theo@jivatechnology.com>
2009-10-26 17:58:36 +0000

    Fix: Now using /usr/local/src for build directories instead of /tmp

--- ejabberd/attributes/default.rb
+++ ejabberd/attributes/default.rb
@@ -2,7 +2,7 @@ ejabberd Mash.new unless attribute?("ejabberd")
 
 ejabberd[:version] = "2.0.5" unless ejabberd.has_key?(:version)
 ejabberd[:link] = "http://www.process-one.net/downloads/ejabberd/#{ejabberd[:version]}/ejabberd-#{ejabberd[:version]}.tar.gz" unless ejabberd.has_key?(:link)
-ejabberd[:destination] = "/tmp" unless ejabberd.has_key?(:destination)
+ejabberd[:destination] = "/usr/local/src" unless ejabberd.has_key?(:destination)
 ejabberd[:folder] = "#{ejabberd[:destination]}/ejabberd-#{ejabberd[:version]}" unless ejabberd.has_key?(:folder)
 
 ejabberd[:hosts] = [ fqdn ] unless ejabberd.has_key?(:hosts)
--- memcachedb/attributes/default.rb
+++ memcachedb/attributes/default.rb
@@ -2,7 +2,7 @@ memcachedb Mash.new unless attribute?("memcachedb")
 
 memcachedb[:version] = "1.2.0" unless memcachedb.has_key?(:version)
 memcachedb[:link] = "http://memcachedb.googlecode.com/files/memcachedb-#{memcachedb[:version]}.tar.gz" unless memcachedb.has_key?(:link)
-memcachedb[:destination] = "/tmp" unless memcachedb.has_key?(:destination)
+memcachedb[:destination] = "/usr/local/src" unless memcachedb.has_key?(:destination)
 memcachedb[:folder] = "#{memcachedb[:destination]}/memcachedb-#{memcachedb[:version]}" unless memcachedb.has_key?(:folder)
 
 memcachedb[:options] = [] unless memcachedb.has_key?(:memcachedb)
