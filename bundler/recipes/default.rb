646ee820e0d00089d8f62d3ed5c1a590168ea6f3
Theo Cushion <theo@jivatechnology.com>
2009-11-09 16:42:50 +0000

    Feature: Simplified bundler gem install - 0.7.0 has now been released

--- bundler/recipes/default.rb
+++ bundler/recipes/default.rb
@@ -1,7 +1,1 @@
-include_recipe "git"
-
-bash "Download and install bundler" do
-  cwd "/tmp"
-  code "git clone git://github.com/wycats/bundler.git && cd bundler && rake package && gem install pkg/bundler-0.7.0.pre.gem --no-rdoc --no-ri && cd .. && rm -rf bundler"
-  not_if "gem specification bundler"
-end
+gem_package "bundler"
