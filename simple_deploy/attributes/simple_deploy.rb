07c5811010fd8355c05e7127a197decec93d7a44
Theo Cushion <theo@jivatechnology.com>
2009-11-12 18:47:16 +0000

    Feature: Added very basic wrapper for the deploy resource

---
+++ simple_deploy/attributes/simple_deploy.rb
@@ -0,0 +1,6 @@
+default[:simple_deploy][:destination]       = nil
+default[:simple_deploy][:repo]              = nil
+default[:simple_deploy][:revision]          = "HEAD"
+default[:simple_deploy][:user]              = "root"
+default[:simple_deploy][:enable_submodules] = true
+default[:simple_deploy][:restart_command]   = nil
